285fdb994a32d0efe0e0aeed08d68929efe50c1f
Mody-bshh <148812168+Mody-bshh@users.noreply.github.com>
2023-10-23 22:33:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-import chalk from 'chalk'
-import { fileURLToPath } from 'url'
-import { watchFile, unwatchFile, readFileSync } from 'fs'
+import chalk from  chalk 
+import { fileURLToPath } from  url 
+import { watchFile, unwatchFile, readFileSync } from  fs 
 
 global.owner = 
  [
-  ['+212605784394', 'NOUR MD', true]
+  [ 201553941991 ,  مودي ال باشا , true]
 ]
 // Put your number here
 // [number, he creator/owner?, he developer?]
@@ -13,78 +13,78 @@ global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 
 // api key here okay
-global.xyro = 'LyrK9JMI7N' // https://api.xyroinee.xyz free limit unlimited
-global.apilol = 'BrunoSobrino_2' // https://api.lolhuman.xyz free limit unlimited by @BrunoSobrino
+global.xyro =  LyrK9JMI7N  // https://api.xyroinee.xyz free limit unlimited
+global.apilol =  BrunoSobrino_2  // https://api.lolhuman.xyz free limit unlimited by @BrunoSobrino
 
 global.APIs = { // API Prefix
-  // name: 'https://website'
-  nrtm: 'https://nurutomo.herokuapp.com',
-  xteam: 'https://api.xteam.xyz'
+  // name:  https://website 
+  nrtm:  https://nurutomo.herokuapp.com ,
+  xteam:  https://api.xteam.xyz 
 }
 global.APIKeys = { // APIKey Here
-  // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b'
+  //  https://website :  apikey 
+   https://api.xteam.xyz :  d90a9e986e18778b 
 }
 
  global.thumbnailUrl = [
-  'https://telegra.ph/file/81260a8b9e8cff26d2b48.jpg', 'https://telegra.ph/file/ac4928f0824a2a0492737.jpg',
-  'https://telegra.ph/file/6359b013bc7e52c3b346f.jpg', 'https://telegra.ph/file/d43c89a5d2da72875ec05.jpg',
-  'https://telegra.ph/file/7d6c0e35f9c8f52715541.jpg', 'https://telegra.ph/file/ef4b742d47e6a9115e2ff.jpg',
-  'https://telegra.ph/file/55e5af5f33fbd57104187.jpg', 'https://telegra.ph/file/af236598456b95884bd15.jpg',
-  'https://telegra.ph/file/de92ed4a729887ffc974c.jpg', 'https://telegra.ph/file/00ce42a193b1dbbf907d4.jpg'
+   https://telegra.ph/file/81260a8b9e8cff26d2b48.jpg ,  https://telegra.ph/file/ac4928f0824a2a0492737.jpg ,
+   https://telegra.ph/file/6359b013bc7e52c3b346f.jpg ,  https://telegra.ph/file/d43c89a5d2da72875ec05.jpg ,
+   https://telegra.ph/file/7d6c0e35f9c8f52715541.jpg ,  https://telegra.ph/file/ef4b742d47e6a9115e2ff.jpg ,
+   https://telegra.ph/file/55e5af5f33fbd57104187.jpg ,  https://telegra.ph/file/af236598456b95884bd15.jpg ,
+   https://telegra.ph/file/de92ed4a729887ffc974c.jpg ,  https://telegra.ph/file/00ce42a193b1dbbf907d4.jpg 
 ]
 
 // Sticker WM
-global.packname = `「 NOUR BOT BY NOUREDDINE 」`
-global.author = ''
-global.thumb = readFileSync('./me.png')
-global.thumb2 = readFileSync('./me2.jpeg')
+global.packname = `「مودي بيحبك فشخ😂♥️」`
+global.author =   
+global.thumb = readFileSync( ./me.png )
+global.thumb2 = readFileSync( ./me2.jpeg )
 global.multiplier = 69 // The higher, The harder levelup
 
 //Text here
-global.me = 'NOUR BOT - 2023'
-global.bott = 'Nour md'
-global.nomorown = '212605784394'
+global.me =  Mody BOT - 2023 
+global.bott =  Nour md 
+global.nomorown =  212605784394 
 // owner put this creator.js
-global.str = '-------------'
-global.l = '「'
-global.r = '」'
-global.wait = '```「▰▰▰▱▱▱▱▱▱▱」 جاري تلبية طلبكم انتظر...```\ninstagam.com/noureddine_ouafy'
-global.eror = '```404 ᴇʀʀᴏʀ```'
+global.str =  ------------- 
+global.l =  「 
+global.r =  」 
+global.wait =  ```「▰▰▰▱▱▱▱▱▱▱」 جاري تلبية طلبكم انتظر...```\nstagram.com/mody_bshhh?igshid=NTc4MTIwNjQ2YQ== 
+global.eror =  ```404 ᴇʀʀᴏʀ``` 
 
 global.rpg = {
   emoticon(string) {
     string = string.toLowerCase()
     let emot = {
-      level: '🧬',
-      limit: '🌌',
-      health: '❤️',
-      exp: '✉️',
-      money: '💵',
-      potion: '🥤',
-      diamond: '💎',
-      common: '📦',
-      uncommon: '🎁',
-      mythic: '🗳️',
-      legendary: '🗃️',
-      pet: '🎁',
-      trash: '🗑',
-      armor: '🥼',
-      sword: '⚔️',
-      wood: '🪵',
-      rock: '🪨',
-      string: '🕸️',
-      horse: '🐎',
-      cat: '🐈',
-      dog: '🐕',
-      fox: '🦊',
-      petFood: '🍖',
-      iron: '⛓️',
-      gold: '👑',
-      emerald: '💚'
+      level:  🧬 ,
+      limit:  🌌 ,
+      health:  ❤️ ,
+      exp:  ✉️ ,
+      money:  💵 ,
+      potion:  🥤 ,
+      diamond:  💎 ,
+      common:  📦 ,
+      uncommon:  🎁 ,
+      mythic:  🗳️ ,
+      legendary:  🗃️ ,
+      pet:  🎁 ,
+      trash:  🗑 ,
+      armor:  🥼 ,
+      sword:  ⚔️ ,
+      wood:  🪵 ,
+      rock:  🪨 ,
+      string:  🕸️ ,
+      horse:  🐎 ,
+      cat:  🐈 ,
+      dog:  🐕 ,
+      fox:  🦊 ,
+      petFood:  🍖 ,
+      iron:  ⛓️ ,
+      gold:  👑 ,
+      emerald:  💚 
     }
-    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
-    if (!results.length) return ''
+    let results = Object.keys(emot).map(v => [v, new RegExp(v,  gi )]).filter(v => v[1].test(string))
+    if (!results.length) return   
     else return emot[results[0][0]]
   }
 }
@@ -92,6 +92,6 @@ global.rpg = {
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
-  console.log(chalk.cyanBright("Update 'config.js'"))
+  console.log(chalk.cyanBright("Update  config.js "))
   import(`${file}?update=${Date.now()}`)
 })
